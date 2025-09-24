e39e7bdf0b91eb657860257854b78ebe87478fb0
Leon Winheim <leon.winheim@kit.edu>
2025-09-24 23:08:06 +0200

    Correct

--- slurm/run_job.sh
+++ slurm/run_job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=test_hpc
 #SBATCH --output=temp/output_%j.log
 #SBATCH --time=00:01:00
-#SBATCH --partition=standard
+#SBATCH --partition=cpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
