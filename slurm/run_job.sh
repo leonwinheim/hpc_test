957047f589a5371ffce74b264001a294a342757f
winheim <leon.winheim@kit.edu>
2025-09-25 08:32:28 +0200

    Correct

--- slurm/run_job.sh
+++ slurm/run_job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=test_hpc
 #SBATCH --output=temp/output_%j.log
 #SBATCH --time=00:01:00
-#SBATCH --partition=gpu
+#SBATCH --partition=gpu_h100
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
